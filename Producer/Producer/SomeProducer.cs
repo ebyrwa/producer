09a56f0fa923eac1f55faa8bc194d9cc3f968306
Erik Byrwa <Eri072219@paycor.com>
2024-11-21 15:04:59 -0500

    updates producer to use contract

--- Producer/Producer/SomeProducer.cs
+++ Producer/Producer/SomeProducer.cs
@@ -1,11 +1,15 @@
-﻿namespace Producer
+﻿using Contracts;
+
+namespace Producer
 {
     public class SomeProducer
     {
         public void Produce()
         {
             // do some work to create transactions
-            // todo: get submodules configured
+            var transaction = new Transaction() { Amount = 1, PostingDate = DateTimeOffset.UtcNow, TransactionId = Guid.NewGuid(), ReferenceNumber = "Sample Reference Number" };
+            
+            // todo: post transaction on service bus
         }
     }
 }
